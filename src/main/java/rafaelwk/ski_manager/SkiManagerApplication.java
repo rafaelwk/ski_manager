5cc55c53e3598fb42ab6bb6f386feece09678bb6
rafaelwk <rafaelwk81@gmail.com>
2020-10-30 15:20:01 +0100

    init project

---
+++ src/main/java/rafaelwk/ski_manager/SkiManagerApplication.java
@@ -0,0 +1,13 @@
+package rafaelwk.ski_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkiManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkiManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rafaelwk/ski_manager/SkiManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package rafaelwk.ski_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkiManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
